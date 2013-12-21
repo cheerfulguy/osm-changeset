8804caa9ea5ebe2cb185b2fb643260c804584546
Abhishek <cheerfulguy@gmail.com>
2013-12-21 18:30:37 -0500

    first iteration complete

---
+++ csvtools.py
@@ -0,0 +1,28 @@
+import csv
+import sys
+
+
+def chooserows(key, val, inputfile, outputfile):
+    outrows = []
+
+    with open(inputfile) as infileh:
+        csvreader = csv.DictReader(infileh, delimiter = "\t")
+        fields = csvreader.fieldnames
+        for row in csvreader:
+            if row[key] == val:
+                outrows.append(row)
+
+    with open(outputfile, "w") as outfileh:
+        writer = csv.DictWriter(outfileh, fields)
+        writer.writerows(outrows)
+
+if __name__ == "__main__":
+
+    # inputfile = sys.argv[1].strip()
+    # outputfile = sys.argv[2].strip()
+
+    key = "@id"
+    val = "19"
+    
+    chooserows(key, val, "map.csv", "mapfilter.csv")
+    
